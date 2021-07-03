e0411ab4adf945024c143b28eb73d2a4bb7045bb
ppp-pp-pp <57984390+ppp-pp-pp@users.noreply.github.com>
2021-07-04 00:28:49 +0700

    Implementasi Room & Undangan

--- Client.py
+++ Client.py
@@ -29,6 +29,7 @@ thread_cli = threading.Thread(target=read_msg, args=(sock_cli,))
 thread_cli.start()
 
 userstate = "LOBBY"
+hasdeck = False
 
 while True:
     if userstate == "LOBBY":
@@ -37,25 +38,39 @@ while True:
                        "Buat deck\n"
                        "Kirim pesan\n"
                        "Buat room\n"
+                       "Terima undangan\n"
                        "Keluar\n"
                        ">> ")
         if option == "Keluar":
             sock_cli.close()
             break
         elif option == "Lihat deck":
-            sock_cli.send(bytes("Lihat deck", "utf-8"))
+            sock_cli.send(bytes(option, "utf-8"))
             time.sleep(1)
         elif option == "Buat deck":
-            sock_cli.send(bytes("Buat deck", "utf-8"))
+            sock_cli.send(bytes(option, "utf-8"))
             userstate = "DECKBUILDING"
             time.sleep(1)
         elif option == "Kirim pesan":
             pass
         elif option == "Buat room":
-            pass
+            if hasdeck:
+                sock_cli.send(bytes(option, "utf-8"))
+                userstate = "ROOM"
+                time.sleep(1)
+            else:
+                print("Silakan buat deck terlebih dahulu")
+        elif option == "Terima undangan":
+            if hasdeck:
+                sock_cli.send(bytes(option, "utf-8"))
+                userstate = "ACCEPTING"
+                time.sleep(1)
+            else:
+                print("Silakan buat deck terlebih dahulu")
     elif userstate == "DECKBUILDING":
         option = input("\nMasukkan 3 angka yang merupakan jumlah kartu "
-                       "Batu, Gunting, dan Kertas yang kamu inginkan:\n")
+                       "Batu, Gunting, dan Kertas yang kamu inginkan:\n"
+                       ">> ")
         newdeck = option.split()
         if len(newdeck) != 3:
             continue
@@ -66,4 +81,31 @@ while True:
             if rock + paper + scissors == 30:
                 sock_cli.send(bytes(option, "utf-8"))
                 userstate = "LOBBY"
+                hasdeck = True
                 time.sleep(1)
+    elif userstate == "ROOM":
+        option = input("\nApa yang ingin anda lakukan?\n"
+                       "Lihat deck\n"
+                       "Undang pemain\n"
+                       "Mulai permainan\n"
+                       "Kembali ke lobi\n"
+                       ">> ")
+        if option == "Lihat deck":
+            sock_cli.send(bytes(option, "utf-8"))
+            time.sleep(1)
+        elif option == "Undang pemain":
+            dest = input("\nMasukkan username yang ingin diundang ke room: \n"
+                         ">> ")
+            sock_cli.send(bytes("Undang " + dest, "utf-8"))
+            time.sleep(1)
+        elif option == "Kembali ke lobi":
+            sock_cli.send(bytes(option, "utf-8"))
+            userstate = "LOBBY"
+    elif userstate == "ACCEPTING":
+        option = input()
+        if option == "Kembali ke lobi":
+            sock_cli.send(bytes(option, "utf-8"))
+            userstate = "LOBBY"
+        else:
+            sock_cli.send(bytes(option, "utf-8"))
+            time.sleep(1)
--- Server.py
+++ Server.py
@@ -22,21 +22,73 @@ def read_msg(clients, sock_cli, addr_cli, user, userlist):
 
         if user.state == "LOBBY":
             msg = data.decode("utf-8").split("|")
-            if len(msg) == 1 and msg[0] == "Lihat deck":
-                send_msg(sock_cli, "\nDeck saat ini:" +
-                         "\nBatu: " + str(user.decklist[0]) +
-                         "\nGunting: " + str(user.decklist[1]) +
-                         "\nKertas: " + str(user.decklist[2]))
-            elif len(msg) == 1 and msg[0] == "Buat deck":
-                send_msg(sock_cli, "\nDeck saat ini:" +
-                                   "\nBatu: " + str(user.decklist[0]) +
-                                   "\nGunting: " + str(user.decklist[1]) +
-                                   "\nKertas: " + str(user.decklist[2]))
-                user.state = "DECKBUILDING"
+            if len(msg) == 1:
+                if msg[0] == "Lihat deck":
+                    send_msg(sock_cli, "\nDeck saat ini:" +
+                             "\nBatu: " + str(user.decklist[0]) +
+                             "\nGunting: " + str(user.decklist[1]) +
+                             "\nKertas: " + str(user.decklist[2]))
+                elif msg[0] == "Buat deck":
+                    send_msg(sock_cli, "\nDeck saat ini:" +
+                                       "\nBatu: " + str(user.decklist[0]) +
+                                       "\nGunting: " + str(user.decklist[1]) +
+                                       "\nKertas: " + str(user.decklist[2]))
+                    user.state = "DECKBUILDING"
+                elif msg[0] == "Buat room":
+                    user.state = "ROOM"
+                elif msg[0] == "Terima undangan":
+                    menu = "\nPilih pemain:\n"
+                    for u in invitationlist[user]:
+                        menu = menu + u + "\n"
+                    menu = menu + "Kembali ke lobi\n"
+                    send_msg(sock_cli, menu)
+                    user.state = "ACCEPTING"
         elif user.state == "DECKBUILDING":
             msg = data.decode("utf-8").split()
             user.decklist = (int(msg[0]), int(msg[1]), int(msg[2]))
             user.state = "LOBBY"
+        elif user.state == "ROOM":
+            msg = data.decode()
+            if msg == "Lihat deck":
+                send_msg(sock_cli, "\nDeck saat ini:" +
+                         "\nBatu: " + str(user.decklist[0]) +
+                         "\nGunting: " + str(user.decklist[1]) +
+                         "\nKertas: " + str(user.decklist[2]))
+            elif msg.startswith("Undang "):
+                msg = msg.replace("Undang ", '')
+                print(msg)
+                if msg == user.username:
+                    send_msg(sock_cli, "Tidak bisa mengundang diri sendiri")
+                    continue
+                for u in userlist:
+                    if msg == u.username:
+                        send_msg(sock_cli, msg + " berhasil diundang")
+                        invitationlist[u].append(user.username)
+                        break
+                else:
+                    send_msg(sock_cli, "Username tidak ditemukan")
+            elif msg == "Kembali ke lobi":
+                for l in invitationlist.values():
+                    if user.username in l:
+                        remove(user.username)
+                user.state = "LOBBY"
+        elif user.state == "ACCEPTING":
+            msg = data.decode()
+            if msg == "Kembali ke lobi":
+                user.state = "LOBBY"
+            else:
+                for u in userlist:
+                    if msg == u.username:
+                        send_msg(sock_cli, "\nPermainan berhasil diterima")
+                        user.state = "ROOM"
+                        break
+                else:
+                    send_msg(sock_cli, "\nUsername tidak ditemukan")
+                    menu = "\nPilih pemain:\n"
+                    for u in invitationlist[user]:
+                        menu = menu + u + "\n"
+                    menu = menu + "Kembali ke lobi"
+                    send_msg(sock_cli, menu)
     sock_cli.close()
     print("Connection closed", addr_cli)
     userlist.remove(user)
@@ -58,6 +110,7 @@ sock_server.listen(5)
 
 clients = {}
 userlist = []
+invitationlist = {}
 
 while True:
     sock_cli, addr_cli = sock_server.accept()
@@ -70,3 +123,4 @@ while True:
     thread_cli.start()
 
     clients[user] = (sock_cli, addr_cli, thread_cli)
+    invitationlist[user] = []
