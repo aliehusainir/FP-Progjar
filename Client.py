87e3e398e5e26323ee2c35647eb1d4d0d5219ab9
ppp-pp-pp <57984390+ppp-pp-pp@users.noreply.github.com>
2021-07-03 22:52:09 +0700

    Implementasi Lihat deck & Buat deck

--- Client.py
+++ Client.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 import threading
+import time
 
 
 def read_msg(sock_cli):
@@ -11,6 +12,15 @@ def read_msg(sock_cli):
         print(data)
 
 
+def check_int(string):
+    try:
+        if int(string) < 0:
+            return False
+        return True
+    except ValueError:
+        return False
+
+
 sock_cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock_cli.connect(("127.0.0.1", 50000))
 sock_cli.send(bytes(sys.argv[1], "utf-8"))
@@ -18,20 +28,42 @@ sock_cli.send(bytes(sys.argv[1], "utf-8"))
 thread_cli = threading.Thread(target=read_msg, args=(sock_cli,))
 thread_cli.start()
 
-while True:
-    option = input("\nApa yang ingin anda lakukan?\n"
-                   "Buat deck\n"
-                   "Kirim pesan\n"
-                   "Buat room\n"
-                   "Keluar\n"
-                   ">> ")
+userstate = "LOBBY"
 
-    if option == "Keluar":
-        sock_cli.close()
-        break
-    elif option == "Buat deck":
-        pass
-    elif option == "Kirim pesan":
-        pass
-    elif option == "Buat room":
-        pass
+while True:
+    if userstate == "LOBBY":
+        option = input("\nApa yang ingin anda lakukan?\n"
+                       "Lihat deck\n"
+                       "Buat deck\n"
+                       "Kirim pesan\n"
+                       "Buat room\n"
+                       "Keluar\n"
+                       ">> ")
+        if option == "Keluar":
+            sock_cli.close()
+            break
+        elif option == "Lihat deck":
+            sock_cli.send(bytes("Lihat deck", "utf-8"))
+            time.sleep(1)
+        elif option == "Buat deck":
+            sock_cli.send(bytes("Buat deck", "utf-8"))
+            userstate = "DECKBUILDING"
+            time.sleep(1)
+        elif option == "Kirim pesan":
+            pass
+        elif option == "Buat room":
+            pass
+    elif userstate == "DECKBUILDING":
+        option = input("\nMasukkan 3 angka yang merupakan jumlah kartu "
+                       "Batu, Gunting, dan Kertas yang kamu inginkan:\n")
+        newdeck = option.split()
+        if len(newdeck) != 3:
+            continue
+        if check_int(newdeck[0]) and check_int(newdeck[1]) and check_int(newdeck[2]):
+            rock = int(newdeck[0])
+            paper = int(newdeck[1])
+            scissors = int(newdeck[2])
+            if rock + paper + scissors == 30:
+                sock_cli.send(bytes(option, "utf-8"))
+                userstate = "LOBBY"
+                time.sleep(1)
--- Server.py
+++ Server.py
@@ -19,17 +19,37 @@ def read_msg(clients, sock_cli, addr_cli, user, userlist):
         data = sock_cli.recv(65535)
         if len(data) == 0:
             break
+
+        if user.state == "LOBBY":
+            msg = data.decode("utf-8").split("|")
+            if len(msg) == 1 and msg[0] == "Lihat deck":
+                send_msg(sock_cli, "\nDeck saat ini:" +
+                         "\nBatu: " + str(user.decklist[0]) +
+                         "\nGunting: " + str(user.decklist[1]) +
+                         "\nKertas: " + str(user.decklist[2]))
+            elif len(msg) == 1 and msg[0] == "Buat deck":
+                send_msg(sock_cli, "\nDeck saat ini:" +
+                                   "\nBatu: " + str(user.decklist[0]) +
+                                   "\nGunting: " + str(user.decklist[1]) +
+                                   "\nKertas: " + str(user.decklist[2]))
+                user.state = "DECKBUILDING"
+        elif user.state == "DECKBUILDING":
+            msg = data.decode("utf-8").split()
+            user.decklist = (int(msg[0]), int(msg[1]), int(msg[2]))
+            user.state = "LOBBY"
     sock_cli.close()
     print("Connection closed", addr_cli)
     userlist.remove(user)
 
 
 def send_bcast(clients, data, sender_addr_cli):
-    pass
+    for sock_cli, addr_cli, _ in clients.values():
+        if not (sender_addr_cli[0] and sender_addr_cli[1] == addr_cli[1]):
+            send_msg(sock_cli, data)
 
 
 def send_msg(sock_cli, data):
-    pass
+    sock_cli.send(bytes(data, "utf-8"))
 
 
 sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
